2f9f08c7152e873cd3ae35c899d64e8e4fdd4dfc
woogii <jhsw0201@naver.com>
2015-11-30 17:39:01 +0900

    feat : Implement travle location view and add files
    - Implement functions that add and drop pin with animation
    - Add flickr client boiler plate code
    - Add object model file

--- VirtualTourist/TravelLocationsViewController.swift
+++ VirtualTourist/TravelLocationsViewController.swift
@@ -24,9 +24,10 @@ class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
     
         longGesture = UILongPressGestureRecognizer(target: self, action: "addAnnotation:")
-        longGesture!.minimumPressDuration = 1.0
+        longGesture!.minimumPressDuration = 0.5
         
         mapView.addGestureRecognizer(longGesture)
+        mapView.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -67,8 +68,6 @@ class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
                 self.bottomInfoView.alpha = 0
                 self.bottomLayout.constant = -20
                 self.view.layoutIfNeeded()
-                // print(self.bottomInfoView.frame.height)   // 83
-                // print(self.mapView.frame.origin.y)        // 64
 
             }, completion: nil)
             
@@ -80,7 +79,7 @@ class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
         let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
         let annotation = MKPointAnnotation()
         annotation.coordinate = newCoordinates
-        annotation.title = ""
+        
         mapView.addAnnotation(annotation)
     }
     
@@ -91,25 +90,158 @@ class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
         
         if pinView == nil {
             pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            pinView!.canShowCallout = true
+            pinView!.canShowCallout = false
             pinView!.pinTintColor = UIColor.redColor()
             //pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
         }
         else {
             pinView!.annotation = annotation
         }
-        
+        pinView?.animatesDrop = true
         return pinView
  
     }
     
     func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        mapView.deselectAnnotation(view.annotation, animated: true)
         
-        let controller = storyboard!.instantiateViewControllerWithIdentifier("PhotoAlbumViewController") as! PhotoAlbumViewController
+        let controller = storyboard!.instantiateViewControllerWithIdentifier("PhotoAlbum") as! PhotoAlbumViewController
         navigationController?.pushViewController(controller, animated: true)
     
     }
     
     
+   
+    
+//    func mapView(mapView: MKMapView, didAddAnnotationViews views : [MKAnnotationView]) {
+//  
+//        for view in views {
+//            
+//            let endFrame = view.frame
+//            
+//            view.frame = CGRectMake(view.frame.origin.x, 0.0, view.frame.size.width, view.frame.size.height)
+//            view.alpha = 0
+//            UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveLinear, animations: {
+//                view.alpha = 1
+//                view.frame = endFrame
+//                
+//                }, completion: nil)
+//            
+//        }
+//        
+//    }
+    
+    
 }
 
+extension TravelLocationsViewController {
+    
+    
+    private var MERCATOR_OFFSET: Double {
+        get {
+            return 268435456
+        }
+    }
+    private var MERCATOR_RADIUS: Double {
+        get {
+            return 85445659.44705395
+        }
+    }
+    
+    private var MAX_GOOGLE_LEVELS: Double {
+        get {
+            return 20.0
+        }
+    }
+    
+    // MARK: -  Private functions
+    private func longitudeToPixelSpaceX (longitude: Double) -> Double {
+        return round(MERCATOR_OFFSET + MERCATOR_RADIUS * longitude * M_PI / 180.0)
+    }
+    
+    private func latitudeToPixelSpaceY (latitude: Double) -> Double {
+        
+        let a = 1 + sinf(Float(latitude * M_PI) / 180.0)
+        let b = 1.0 - sinf(Float(latitude * M_PI / 180.0)) / 2.0
+        
+        return round(MERCATOR_OFFSET - MERCATOR_RADIUS * Double(logf(a / b)))
+    }
+    
+    private func pixelSpaceXToLongitude (pixelX: Double) -> Double {
+        return ((round(pixelX) - MERCATOR_OFFSET) / MERCATOR_RADIUS) * 180.0 / M_PI
+    }
+    
+    private func pixelSpaceYToLatitude (pixelY: Double) -> Double {
+        return (M_PI / 2.0 - 2.0 * atan(exp((round(pixelY) - MERCATOR_OFFSET) / MERCATOR_RADIUS))) * 180.0 / M_PI
+    }
+    
+    private func coordinateSpanWithMapView(mapView: MKMapView, centerCoordinate: CLLocationCoordinate2D, andZoomLevel zoomLevel:Int) -> MKCoordinateSpan {
+        
+        // convert center coordiate to pixel space
+        let centerPixelX = self.longitudeToPixelSpaceX(centerCoordinate.longitude)
+        let centerPixelY = self.latitudeToPixelSpaceY(centerCoordinate.latitude)
+        
+        // determine the scale value from the zoom level
+        let zoomExponent = 20 - zoomLevel
+        let zoomScale = CGFloat(pow(Double(2), Double(zoomExponent)))
+        
+        // scale the map’s size in pixel space
+        let mapSizeInPixels = mapView.bounds.size
+        let scaledMapWidth = mapSizeInPixels.width * zoomScale
+        let scaledMapHeight = mapSizeInPixels.height * zoomScale
+        
+        // figure out the position of the top-left pixel
+        let topLeftPixelX = CGFloat(centerPixelX) - (scaledMapWidth / 2)
+        let topLeftPixelY = CGFloat(centerPixelY) - (scaledMapHeight / 2)
+        
+        // find delta between left and right longitudes
+        let minLng: CLLocationDegrees = self.pixelSpaceXToLongitude(Double(topLeftPixelX))
+        let maxLng: CLLocationDegrees = self.pixelSpaceXToLongitude(Double(topLeftPixelX + scaledMapWidth))
+        let longitudeDelta: CLLocationDegrees = maxLng - minLng
+        
+        // find delta between top and bottom latitudes
+        let minLat: CLLocationDegrees = self.pixelSpaceYToLatitude(Double(topLeftPixelY))
+        let maxLat: CLLocationDegrees = self.pixelSpaceYToLatitude(Double(topLeftPixelY + scaledMapHeight))
+        let latitudeDelta: CLLocationDegrees = -1 * (maxLat - minLat)
+        
+        // create and return the lat/lng span
+        let span = MKCoordinateSpanMake(latitudeDelta, longitudeDelta)
+        
+        return span
+        
+    }
+    
+    // MARK: - Public Functions
+    
+    func setCenterCoordinate(centerCoordinate: CLLocationCoordinate2D, zoomLevel: Int, animated: Bool) {
+        
+        // clamp large numbers to 28
+        let zoom = min(zoomLevel, 28)
+        
+        // use the zoom level to compute the region
+        let span = self.coordinateSpanWithMapView(self.mapView, centerCoordinate:centerCoordinate, andZoomLevel:zoom)
+        let region = MKCoordinateRegionMake(centerCoordinate, span)
+        
+        // set the region like normal
+        self.mapView.setRegion(region, animated:animated)
+        
+    }
+    
+    func getZoomLevel() ->Double {
+    
+        
+    
+        let longitudeDelta:CLLocationDegrees = self.mapView.region.span.longitudeDelta
+        let mapWidthInPixels:CGFloat  = self.mapView.bounds.size.width
+        let zoomScale = (longitudeDelta * MERCATOR_RADIUS * M_PI) / Double(180.0 * mapWidthInPixels)
+        var zoomer = MAX_GOOGLE_LEVELS - log2( zoomScale )
+        
+        if ( zoomer < 0 ) {
+            zoomer = 0
+        }
+    
+        return zoomer
+    }
+    
+    
+}
